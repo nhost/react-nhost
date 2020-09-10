65cc92889e5601d47850e3588e98698500d1b0e6
elitan <johan@eliasson.me>
2020-09-10 13:03:42 +0200

    lazy and correct ws reconnect

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ export function generateApolloClient(
         uri: wsUri,
         options: {
           reconnect: true,
+          lazy: true,
           connectionParams: () => {
             const connectionHeaders = getheaders(auth);
             return {
@@ -106,17 +107,41 @@ export class NhostApolloProvider extends React.Component {
     this.client = client;
     this.wsLink = wsLink;
 
+    // for debug
+    // const ssr = typeof window === "undefined";
+    // if (!ssr) {
+    //   console.log(" not ssr, set up .on");
+    //   this.wsLink.subscriptionClient.on("connecting", () =>
+    //     console.log("connecting")
+    //   );
+    //   this.wsLink.subscriptionClient.on("connected", () =>
+    //     console.log("connected")
+    //   );
+    //   this.wsLink.subscriptionClient.on("reconnecting", () =>
+    //     console.log("reconnecting")
+    //   );
+    //   this.wsLink.subscriptionClient.on("reconnected", () =>
+    //     console.log("reconnected")
+    //   );
+    //   this.wsLink.subscriptionClient.on("disconnected", () =>
+    //     console.log("disconnected")
+    //   );
+    //   this.wsLink.subscriptionClient.on("error", () => console.log("error"));
+    // }
+
     if (this.props.auth) {
       this.props.auth.onTokenChanged(() => {
         if (this.wsLink.subscriptionClient.status === 1) {
           this.wsLink.subscriptionClient.tryReconnect();
-        } else {
         }
       });
 
       this.props.auth.onAuthStateChanged((data) => {
-        // reconnect ws connection with new auth headers for the logged in user
+        // reconnect ws connection with new auth headers for the logged in/out user
         if (this.wsLink.subscriptionClient.status === 1) {
+          // must close first to avoid race conditions
+          this.wsLink.subscriptionClient.close();
+          // reconnect
           this.wsLink.subscriptionClient.tryReconnect();
         }
       });
